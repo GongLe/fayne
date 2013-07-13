52c6bae75b6dce0efe97d941458f6e923e58aec3
Gongle <skygongle@gmail.com>
2013-07-13 11:54:39 +0800

    完善项目结构.

---
+++ modules/runner/src/test/java/org/fayne/Main.java
@@ -0,0 +1,12 @@
+package org.fayne;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Gongle
+ * Date: 13-7-13
+ * Time: 上午11:52
+ * To change this template use File | Settings | File Templates.
+ */
+public class Main {
+
+}
